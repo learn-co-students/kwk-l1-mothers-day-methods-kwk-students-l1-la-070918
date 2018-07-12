7dfec80e5c2ed51f091ed501ff0129e5285e0748
elizabethlazear <elizabethlazear04@gmail.com>
2018-07-12 17:10:18 +0000

    Done.

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,3 +1,26 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
+def greetings(greetings="Happy Mothers Day Mommy!")
+  puts "#{greetings}"
+end 
+greetings
+
+def personalized(personalized="Happy Mothers Day Grammie!")
+  puts "#{personalized}"
+end
+personalized
+
+
+
+def mothers_day(mom="Mommy")
+  puts "Happy Mothers Day #{mom}!"
+end
+
+mothers_day
+
+
+def your_name(your_name)
+  puts "hi #{your_name}
+end
+your_name="jen"
 
